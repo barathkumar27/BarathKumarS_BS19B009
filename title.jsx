9c85da395337ad92b262b3596e0723429415ee94
barath <sbarathkumar27@gmail.com>
2021-07-19 22:38:25 +0530

    added 1 html & 2 jsx files

---
+++ title.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//func compo
+function func(){
+    return(
+    <div>
+            <div>Timber</div>
+            <div>text</div>
+        </div>
+    );
+}
+
+export default func;
